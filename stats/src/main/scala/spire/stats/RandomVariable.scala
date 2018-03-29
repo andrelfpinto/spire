5c8404f83e63e167d8957fe9da37e1263ebee033
André L. F. Pinto <andreluizferreirapinto@gmail.com>
2018-03-29 10:41:39 -0300

    Establish basic blocks for new spire.stats package
    In the present structure, random.Dist is a way of producing arbitrary
    values from a Generator. However a probability distribution should be a
    way of assign a probability to each measurable subset of the possible
    outcomes of a random experiment [1], i.e. you should be able to ask it
    for pdf, cdf, mean etc. The mechanism for production of values according
    to a probability distribution function should in reality be a random
    variable [2].
    With the proposed separation it should be possible to characterize a
    probability distribution and still be able to modify the production of
    values from the Generator (the role of random.Dist would basically go to
    stats.RandomVariable).
    stats.Distribution and stats.RandomVariable would be immutable.
    
    [1] http://en.wikipedia.org/wiki/Probability_distribution
    [2] http://en.wikipedia.org/wiki/Random_variable

---
+++ stats/src/main/scala/spire/stats/RandomVariable.scala
@@ -0,0 +1,46 @@
+package spire.stats
+
+import scala.reflect.ClassTag
+import spire.random.Generator
+import spire.stats.distribution.Distribution
+
+sealed abstract class RandomVariable[A] {
+
+  /**
+   * Generates a random number according to this variable's probability
+   * distribution function
+   */
+  def observe(gen: Generator): A
+
+  /**
+   * Generates an array of random numbers according to this variable's
+   * probability distribution function
+   */
+  def observeMany(gen: Generator, count: Int)(implicit ev: ClassTag[A]): Array[A] = {
+    val arr = Array.ofDim[A](count)
+    fill(gen, arr)
+
+    arr
+  }
+
+  /**
+   * Fills an array of random numbers according to this variable's probability
+   * distribution function
+   */
+  def fill(gen: Generator, arr: Array[A]): Unit = {
+    var i = 0
+    while (i < arr.length) {
+      arr(i) = observe(gen)
+      i += 1
+    }
+  }
+}
+
+object RandomVariable {
+  def apply[A, B](distribution: Distribution[A, B]): RandomVariable[A] =
+    new RandomVariableWithDistribution(distribution)
+}
+
+private class RandomVariableWithDistribution[A, B](distribution: Distribution[A, B]) extends RandomVariable[A] {
+  def observe(gen: Generator): A = distribution.transform(gen)
+}
